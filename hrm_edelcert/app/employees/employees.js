1a7c6fa102cf914330c759114cc71f007d238580
lkp1996 <lukeperrottet@gmail.com>
2017-03-24 15:55:42 +0100

    Added Ajax calls

--- hrm_edelcert/app/employees/employees.js
+++ hrm_edelcert/app/employees/employees.js
@@ -9,49 +9,14 @@ angular.module('myApp.employees', ['ngRoute'])
         });
     }])
 
-    .controller('EmployeesCtrl', ['$scope', function ($scope) {
+    .controller('EmployeesCtrl', ['$scope', '$http', function ($scope, $http) {
 
-        $scope.employees = [
-            {
-                "id": 1,
-                "lastName": "Perrottet",
-                "firstName": "Luke",
-                "phone": "0787716628",
-                "email": "lukeperrottet@gmail.com",
-                "currentTitle": "Informaticien"
-            },
-            {
-                "id": 2,
-                "lastName": "Tartampion",
-                "firstName": "Jules",
-                "phone": "0787716628",
-                "email": "tartampionj@gmail.com",
-                "currentTitle": "asdf"
-            },
-            {
-                "id": 3,
-                "lastName": "Test",
-                "firstName": "Test",
-                "phone": "0787716628",
-                "email": "test@gmail.com",
-                "currentTitle": "test"
-            },
-            {
-                "id": 4,
-                "lastName": "Jordan",
-                "firstName": "Michael",
-                "phone": "0787716628",
-                "email": "jordanm@gmail.com",
-                "currentTitle": "fdsa"
-            },
-            {
-                "id": 5,
-                "lastName": "James",
-                "firstName": "LeBron",
-                "phone": "0787716628",
-                "email": "james@gmail.com",
-                "currentTitle": "qetrwrew"
+        $scope.employees = [];
+
+        $http.get("http://localhost/hrm_edelcert_server/ctrl/ctrl.php?employees_list").then(
+            function (data) {
+                $scope.employees = data.data;
             }
-        ];
+        );
 
     }]);
